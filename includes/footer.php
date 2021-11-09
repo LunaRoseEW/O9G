d0008f5685b4de65b3b794ffcdf570c6729ede44
root <christianh82995@gmail.com>
2021-11-08 22:51:19 -0600

    navbar_footer_and_head_includes

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<div class="footer bg-dark text-white btn-outline-light form-white fixed-bottom">
+			<p>MyViewers &copy; <?php echo date('Y'); ?></p>
+		</div>
+		<!-- // footer -->
+
+	</div>
+	<!-- // container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ includes/head_section.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+	<!-- Styling for public area -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="static/css/public_styling.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
