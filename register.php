36581a6f9314a235df67b3c1fa672f9fa6bf550a
root <christianh82995@gmail.com>
2021-11-16 16:04:46 -0600

    fixed error bug and set up for admin side

--- register.php
+++ register.php
@@ -15,8 +15,7 @@
 	<div style="width: 40%; margin: 20px auto;">
 		<form method="post" action="register.php" >
 			<h2>Register on Obnoxious 9 Games</h2>
-			<!--This is bugged
-                <?p hp include(ROOT_PATH . '/includes/errors.php'); ?>-->
+            <?php include(ROOT_PATH . '/includes/errors.php'); ?>
 			<input  type="text" name="username" value="<?php echo $username; ?>"  placeholder="Username">
 	 		<input type="email" name="email" value="<?php echo $email ?>" placeholder="Email">
 			<input type="password" name="password_1" placeholder="Password">
