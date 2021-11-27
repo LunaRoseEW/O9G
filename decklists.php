a5d07c847f7a2952af83e5c794b76f77b9fbcee3
root <christianh82995@gmail.com>
2021-11-26 23:02:13 -0600

    added includes for decklists.php

---
+++ decklists.php
@@ -0,0 +1,17 @@
+<?php  include('config.php'); ?>
+<?php include('includes/head_section.php'); ?>
+
+<title>Obnoxious 9 Games | Decklists </title>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+		<!-- Navbar -->
+			<?php include( ROOT_PATH . '/includes/navbar.php'); ?>
+		<!-- // Navbar -->
+<div class="container">
+
+</div>
+<!-- // container -->
+<!-- Footer -->
+<?php include( ROOT_PATH . '/includes/footer.php'); ?>
+<!-- // Footer -->
