efc2d926f1c12d0a64069567fef34c99652094e0
root <christianh82995@gmail.com>
2021-11-27 21:10:17 -0600

    fixed text overflow bug with ellipses

--- includes/head_section.php
+++ includes/head_section.php
@@ -11,6 +11,6 @@
 
 	<!-- Styling for public area -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<link href="static/css/public_styling.css?nocache={timestamp}" rel="stylesheet">
+	<link href="static/css/public_styling.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
