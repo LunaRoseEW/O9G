c7d2f82cf488f3003c48dab443fde248c7f971df
root <christianh82995@gmail.com>
2022-01-12 18:27:24 -0600

    added styling for background and cards and fixed seeral bugs

--- admin/includes/post_functions.php
+++ admin/includes/post_functions.php
@@ -166,7 +166,7 @@ array_push($errors, "Failed to upload image. Please check file settings for your
 
 // register topic if there are no errors in the form
 if (count($errors) == 0) {
-$query = "UPDATE post SET title='$title', slug='$post_slug', views=0, image='$featured_image', body='$body', published=$published, updated_at=now() WHERE id=$post_id";
+$query = "UPDATE post SET title='$title', slug='$post_slug', views=0, body='$body', published=$published, updated_at=now() WHERE id=$post_id";
 // attach topic to post on post_topic table
 if(mysqli_query($conn, $query)){ // if post created successfully
 if (isset($topic_id)) {
