d399c801c7082084f021e08da5609facb9595521
root <christianh82995@gmail.com>
2022-01-16 00:18:29 -0600

    added terms of use

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,7 @@
 
 	<footer class="mt-auto bg-dark text-light">
 			<div class="container text-center">
-				<p>Obnoxious 9 Games &copy; <?php echo date('Y'); ?></p>
+				<p>Obnoxious 9 Games &copy; <?php echo date('Y'); ?> <a href='/terms.php' class='link-light'>	Terms of Use</a></p> 
     			<a href="https://www.facebook.com/Obnoxious-9-Games-106512041821422/?ref=pages_you_manage"><i class="fab fa-facebook fa-2x greenIcon"></i></a>
     			<a href="https://twitter.com/Obnoxious9Games"><i class="fab fa-twitter fa-2x pinkIcon"></i></a>
     			<a href="https://www.youtube.com/channel/UCdsiiI1rQZ08nOmEhjTYwNg"><i class="fab fa-youtube fa-2x greenIcon"></i></a>
---
+++ terms.php
@@ -0,0 +1,60 @@
+<?php  include('config.php'); ?>
+
+<?php include('includes/head_section.php'); ?>
+<title> Obnoxious 9 Games | Terms of Use</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+
+	<!-- Navbar -->
+	<?php include( ROOT_PATH . '/includes/navbar.php'); ?>
+	<!-- // Navbar -->
+
+<div class="container">
+	
+	<div class="row justify-content-center mt-5" >
+		<!-- Page wrapper -->
+		<div class="col-sm-12">
+			<!-- full post div -->
+			<div class="card shadow-lg rounded mb-5">
+				<div class='ms-5 me-5'>
+                    <div class='card-title'>
+                        <h1 class='mt-3'>Terms of Use</h1>
+                        <h4>By using or viewing our site you agree to the following terms:</h4>
+                    </div>
+                    <div class='card-body'>
+                        <ul>
+<li class='mb-3'>Obnoxious9Games.com is a private company offering this site a resource to you free of charge.</li>
+
+<li class='mb-3'>Metazoo games LLC owns the rights to all pictures characters and everything else that they produce, this site exists merely to discuss them and grow the community in a positive manner
+If you violate this we are not liable or responsible for damages, expenses or claims you may incur, nor may you attempt to hold us or our lawyers liable for the same.</li>
+
+<li class='mb-3'>All information given on this site is the opinion of our individual writers and is not necessarily the opinion of obnoxious9games.com as a whole and is provided as it is written and may contain errors.</li>
+
+<li class='mb-3'>Any finance or price driven advice or investment articles are entirely speculative and meant for entertainment purposes, we are not liable if you loose money on an investment, just as we will not attempt to collect from you if you have financial gains.</li>
+
+<li class='mb-3'>When using our sites deck list upload feature, do not use words that are not card names, ie. profanity expletives racial slurs sexual references etc, our software will find them and delete them, and you will lose your privileges.</li>
+
+<li class='mb-3'>We will not tolerate bullying or manipulation of any kind.</li>
+
+<li class='mb-3'>By visiting our site we are not liable for any bugs or site failures, limitations or anything else, including ANY loss or damages of any kind whether they be corporate, personal or any other kind.</li>
+
+<li class='mb-3'>By using this site we are not liable for your use of the information provided and can not be held responsible or liable for anything relating to its use, and we are indemnified from claims, and expenses as are our lawyers, subsidiaries, partners, and affiliates from your use of the information on this site.</li>
+
+<li class='mb-3'>We hold the right to change or alter the terms of use without any notice due to adding new features, changing features, adding premium service levels, etc.</li>
+</ul>
+                    </div>
+			    </div>
+			</div>
+			<!-- // full post div -->
+			
+			<!-- comments section -->
+			<!--  coming soon ...  -->
+		</div>
+		<!-- // Page wrapper -->
+
+	</div>
+</div>
+<!-- // content -->
+
+<?php include( ROOT_PATH . '/includes/footer.php'); ?>
